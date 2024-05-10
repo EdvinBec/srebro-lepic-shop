884ee4f0580d39b61e78400365cfb276fa54c944
Edvin Becic <edvinbecic16@outlook.com>
2024-05-10 15:32:36 +0200

    add heading component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import Heading from "@/components/Heading";
 import HeroSection from "@/components/HeroSection";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
     <div className="bg-slate-50">
       <HeroSection />
+      <MaxWidthWrapper>
+        <section>
+          <Heading className="mt-10 md:mt-16">Naše preporuke</Heading>
+        </section>
+      </MaxWidthWrapper>
     </div>
   );
 }
---
+++ components/Heading.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import React from "react";
+import Star from "@/public/assets/Star.svg";
+import { cn } from "@/lib/utils";
+
+type Props = {
+  children?: React.ReactNode;
+  className?: string;
+};
+
+const Heading = ({ children, className }: Props) => {
+  return (
+    <div className={cn("w-full flex justify-center", className)}>
+      <div className="flex items-start gap-5 md:gap-14">
+        <Image src={Star} alt="star" className="h-[70px] md:h-full" />
+        <h1 className="font-boska font-bold uppercase text-3xl md:text-5xl">
+          {children}
+        </h1>
+        <Image src={Star} alt="star" className="h-[70px] md:h-full" />
+      </div>
+    </div>
+  );
+};
+
+export default Heading;
