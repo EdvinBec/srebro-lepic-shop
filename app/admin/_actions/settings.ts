72f2958cb703932ed114d2e08f2e6f73b17eb1f6
Edvin Becic <edvinbecic16@outlook.com>
2024-07-24 12:30:03 +0200

    minor fix

--- app/admin/_actions/settings.ts
+++ app/admin/_actions/settings.ts
@@ -2,19 +2,6 @@
 
 import db from "@/db/db";
 
-export const onSubmit = async (amount: number) => {
-  if (await db.shopSettings.findUnique({ where: { id: 1 } })) {
-    await db.shopSettings.update({
-      where: { id: 1 },
-      data: { deliveryFee: Number(amount) },
-    });
-  } else {
-    await db.shopSettings.create({
-      data: { deliveryFee: Number(amount), attentionMessage: "" },
-    });
-  }
-};
-
 export const saveAttentionRowMessage = async (message: string) => {
   if (await db.shopSettings.findUnique({ where: { id: 1 } })) {
     await db.shopSettings.update({
@@ -23,7 +10,7 @@ export const saveAttentionRowMessage = async (message: string) => {
     });
   } else {
     await db.shopSettings.create({
-      data: { deliveryFee: Number(0), attentionMessage: message },
+      data: { attentionMessage: message },
     });
   }
 };
